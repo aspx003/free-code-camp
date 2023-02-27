b1b2a86c2ffe0d8e91ee6ab28325b90e88355a32
Ansuman Patra <ansupatra003@gmail.com>
2023-02-27 15:38:17 +0530

    UseEffect Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-// import Setup from './tutorial/1-use-state/1-error-example'
-// import Setup from './tutorial/1-use-state/2-use-state-basics'
-// import Setup from './tutorial/1-use-state/3-useState-array'
-// import Setup from './tutorial/1-use-state/4-useState-object'
-// import Setup from './tutorial/1-use-state/5-useState-counter'
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 
 
 function App() {
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,26 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+
+    if (value > 0) {
+      document.title = `New Messages(${value})`
+    }
+    console.log('Use Effect 1 ran!');
+  }, [value]); // - > Dependency list
+  // [] -> Runs only once, on the initial render
+  // [value] -> runs only when the value is changed
+
+  // can also add multiple useEffects..
+  useEffect(() => {
+    console.log("UseEffect 2 ran!");
+  }, []);
+  return <>
+    <h2>{value}</h2>
+    <button className='btn' onClick={() => { setValue(value + 1) }}>Click here</button>
+  </>;
 };
 
 export default UseEffectBasics;
